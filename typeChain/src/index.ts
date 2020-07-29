62efda214c0d4cc1b03448a544af755927c880b3
ghdmsrkd <ghdmsrkd@naver.com>
2020-07-29 14:50:38 +0900

    #0.8 Blockchain Creating a Block

--- typeChain/src/index.ts
+++ typeChain/src/index.ts
@@ -1,22 +1,23 @@
-class Human {
-    public name: string;
-    public age: number;
-    public gender: string;
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
 
-    constructor(name: string, age: number, gender: string) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+    constructor(index: number, hash: string, previousHash: string, data: string, timestamp: number) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
 
-const eunho = new Human("eunho", 24, "male");
+const genesisBlock: Block = new Block(0, "20202020202020", "", "Hello", 20200729);
 
-const sayHi = (person: Human): string => {
-    return person.name + ":" + person.age;
-};
+let blockchain: [Block] = [genesisBlock];
 
-console.log(sayHi(eunho));
-
+console.log(blockchain);
 
 export { };

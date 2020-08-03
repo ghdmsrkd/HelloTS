79fa34e128ce8dce52263a076e12ed729cb4435e
ghdmsrkd <ghdmsrkd@naver.com>
2020-08-03 18:03:25 +0900

    #2.3 map Recap

--- movie_app_ts/movie_app_ts/src/App.tsx
+++ movie_app_ts/movie_app_ts/src/App.tsx
@@ -1,24 +1,47 @@
 import * as React from 'react';
 
 interface foodProps {
-  fav: string
+  name: string,
+  image : string
 }
+
+const foods : foodProps[] = [
+  {
+    name : "kimchi",
+    image : "https://placeimg.com/200/200/10/10/any/0"
+  },
+  {
+    name : "chicken",
+    image : "https://placeimg.com/200/200/10/10/any/1"
+  },
+  {
+    name : "ramen",
+    image : "https://placeimg.com/200/200/10/10/any/2"
+  },
+  {
+    name : "gogii",
+    image : "https://placeimg.com/200/200/10/10/any/3"
+  }
+];
 
 function Food(props: foodProps) {
   return (
     <h2>
-      I love {props.fav}
+      I love {props.name}<p/>
+      <img src={props.image}/>
     </h2>
   );
+}
+
+function renderFood(dish : foodProps, key: number) {
+  return <Food name={dish.name} image={dish.image} key={key} />;
 }
 
 function App() {
   return (
     <div className="App">
-      <Food fav={"kimchi"} />
-      <Food fav={"chicken"} />
-      <Food fav={"ramen"} />
-      <Food fav={"gogii"} />
+      {console.log(foods.map((food, i) => renderFood(food, i)))}
+      {foods.map((food, i) => renderFood(food, i))}
     </div>
   );
 }

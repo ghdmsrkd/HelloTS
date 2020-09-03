abcbd72800d4560659fcdd4fd2f82d142469d3cc
ghdmsrkd <ghdmsrkd@naver.com>
2020-09-03 19:07:55 +0900

    add auction & filter

--- helloEJS/app.ts
+++ helloEJS/app.ts
@@ -1,4 +1,4 @@
-import express, {Request, Response, NextFunction} from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import path from 'path';
 import indexRouter from "./routes/index";
 
@@ -11,25 +11,35 @@ const app = express();
 //라이브리로드 설정
 const liveServer = livereload.createServer({
   // 변경시 다시 로드할 파일 확장자들 설정
-  exts: ['html', 'css', 'ejs'],
+  exts: ['html', 'css', 'ejs', 'js', 'ts'],
   debug: true
 });
 liveServer.watch(__dirname);
 app.use(connectLivereload());
 
-app.set('views', path.join(__dirname,'views'));
-app.set('view engine','ejs');  // 템플릿 엔진 설정
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');  // 템플릿 엔진 설정
 
 
 
-app.use('/',indexRouter);
+app.use('/', indexRouter);
 app.use(express.static('public'));
 
 //정적 파일 설정
 app.use('/public', express.static(__dirname + '/public'));
 
 
-app.listen(8001,()=>{  // 포트번호 설정
+
+// const appDir = path.dirname(__dirname);
+// console.log(appDir)
+// app.use((request, response, next) => 
+// { 
+//     response.locals.someVar = 'value of someVar'; 
+//     response.locals.appDir = appDir;
+//     return next();
+// });
+
+app.listen(8001, () => {  // 포트번호 설정
   console.log('8001번 포트에서 서버 대기중입니다!');
 });
 
--- helloEJS/routes/index.ts
+++ helloEJS/routes/index.ts
@@ -4,28 +4,32 @@ import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
 
 const router = express.Router();
 
-router.get('/', async (req, res) =>{
+router.get('/', async (req, res) => {
     res.render('main', {});
 })
 
-router.get('/account/signup', async (req, res) =>{
-    res.render('signup', {});
+router.get('/account/signup', async (req, res) => {
+    res.render('account/signup', {});
 })
 
-router.get('/account/emailCheck', async (req, res) =>{
-    res.render('emailCheck', {});
+router.get('/account/emailCheck', async (req, res) => {
+    res.render('account/emailCheck', {});
 })
 
-router.get('/account/login', async (req, res) =>{
-    res.render('login', {});
+router.get('/account/login', async (req, res) => {
+    res.render('account/login', {});
 })
 
-router.get('/account/findIdChecked', async (req, res) =>{
-    res.render('findIdChecked', {});
+router.get('/account/findIdChecked', async (req, res) => {
+    res.render('account/findIdChecked', {});
 })
 
-router.get('/account/findPw', async (req, res) =>{
-    res.render('findPw', {});
+router.get('/account/findPw', async (req, res) => {
+    res.render('account/findPw', {});
+})
+
+router.get('/account/findPwChecked', async (req, res) => {
+    res.render('account/findPwChecked', {});
 })
 
 //알림 기능은 추후 개발 예정
@@ -33,7 +37,7 @@ router.get('/account/findPw', async (req, res) =>{
 //     res.render('noticeList', {});
 // })
 
-router.get('/auction/product_list', async (req, res) =>{
+router.get('/auction/product/list', async (req, res) => {
     res.render('auction/productList', {});
 })
 
